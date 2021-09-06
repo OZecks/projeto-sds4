862d8ba4a8714b55891970b1b59e6881587b17c4
Ezequiel Alves <ezequiel.alves.souza06@gmail.com>
2021-09-06 18:12:07 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary"> Olá Mundo!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
