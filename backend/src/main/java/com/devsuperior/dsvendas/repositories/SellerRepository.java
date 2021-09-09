ea266809d84ea076932359b4ae1466d7c8ab8258
Ezequiel Alves <ezequiel.alves.souza06@gmail.com>
2021-09-08 21:27:12 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsvendas.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsvendas.entities.Seller;
